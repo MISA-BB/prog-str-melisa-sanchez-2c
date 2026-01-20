596c7697c01a6832e0194b619159774d644b18b6
MISA-BB <20253ds083@utez.edu.mx>
2026-01-19 19:55:26 -0600

    feat: agregar descuento por umbral

--- GitConflictoSanchezMelisa/src/Main.java
+++ GitConflictoSanchezMelisa/src/Main.java
@@ -1,18 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-
+    public static final double UMBRAL_DESCUENTO = 1000.0;
+    public static final double DESCUENTO = 0.10;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
         double total = subtotal;
-
-        System.out.printf("Total a pagar: %.2f%n", total);
+        if (subtotal > UMBRAL_DESCUENTO){
+            total = total - (total * DESCUENTO);
+            System.out.printf("Total a pagar: %.2f%n", total);
+        } else {
+            System.out.printf("Total a pagar: %.2f%n", total);
+        }
     }
 
     public static double pedirDouble(Scanner scanner, String mensaje) {
         System.out.print(mensaje);
         return scanner.nextDouble();
     }
+
 }
